e40887f390db6d62a2037478443fedfe860dd449
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-16 17:53:38 +0900

    finish 3.3

--- ch03/3.3_Use_logistic_for_prediction.py
+++ ch03/3.3_Use_logistic_for_prediction.py
@@ -117,3 +117,31 @@ print(lr.predict_proba(X_test_std[:3,:]))
 print(lr.predict(X_test_std[:3, :]))
 print(lr.predict(X_test_std[0, :].reshape(1, -1)))
 print('---\n')
+
+print('3.3.5-正則化による過学習への対処')
+# figに保存
+fig = plt.figure(5)
+print('fig5')
+# 空のリストを生成(重み係数、正則化パラメータ)
+weights, params = [], []
+# 10個の逆正則化パラメータに対応するロジスティック回帰モデルをそれぞれ処理
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10.**c, random_state=1,
+                            solver='lbfgs', multi_class='ovr')
+    lr.fit(X_train_std, y_train)
+    # 重み係数を格納
+    weights.append(lr.coef_[1])
+    # 逆正則化パラメータを格納
+    params.append(10.**c)
+
+# 重み係数をNumpy配列に変換
+weights = np.array(weights)
+# 横軸に逆正則化パラメータ、縦軸に重み係数をプロット
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+# 横軸を対数スケールに設定
+plt.xscale('log')
+plt.show()
