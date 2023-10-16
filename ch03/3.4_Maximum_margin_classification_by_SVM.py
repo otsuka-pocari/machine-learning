f2a7af6554f5b55214a25d81f186a0db163e26e7
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-16 18:13:12 +0900

    start 3.4

---
+++ ch03/3.4_Maximum_margin_classification_by_SVM.py
@@ -0,0 +1,43 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+import numpy as np
+import matplotlib.pyplot as plt
+from plot_decision_regions import plot_decision_regions
+
+# 3.3章までで必要なコードの引用
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train== 1)]
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+print('3.4.2-スラック変数を使った線形分離不可能なケースへの対処')
+# figの保存
+fig = plt.figure(1)
+print('fig1')
+# 線形SVMのインスタンスを生成
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+# 線形SVMのモデルに訓練データを適合させる
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+print('---\n')
+
+plt.tight_layout()
+plt.show()
