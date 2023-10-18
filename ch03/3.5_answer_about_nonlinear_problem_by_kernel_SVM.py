7059adecf05c4917de63282cc534d97e28f0e66e
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-18 14:18:59 +0900

    finish 3.5

--- ch03/3.5_answer_about_nonlinear_problem_by_kernel_SVM.py
+++ ch03/3.5_answer_about_nonlinear_problem_by_kernel_SVM.py
@@ -1,7 +1,32 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.svm import SVC
+from plot_decision_regions import plot_decision_regions
+
+# 3.4章までで必要なコードの引用
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train== 1)]
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
 
 print('3.5.1-線形分離不可能なデータに対するカーネル手法')
+# figに保存
+fig = plt.figure(1)
+print('fig1')
 # 乱数シードを指定
 np.random.seed(1)
 # 標準正規分布に従う乱数で200行2列の行列を生成
@@ -18,5 +43,37 @@ plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1], c='r', marker='s', label='
 plt.xlim([-3, 3])
 plt.ylim([-3, 3])
 plt.legend(loc='best')
+print('---')
+print('3.5.2-カーネルトリックを使って分離超平面を高次元空間で特定する')
+# figに保存
+fig = plt.figure(2)
+print('fig2')
+# RBFカーネルによるSVMのインスタンスを生成
+svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+# figに保存
+fig = plt.figure(3)
+print('fig3')
+# RBFコーネルによるSVMのインスタンスを生成
+svm = SVC(kernel='rbf', random_state=1, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std,y_combined, classifier=svm,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+# figに保存
+fig = plt.figure(4)
+print('fig4')
+# RBFカーネルによるSVMのインスタンスを生成(γパラメータを変更)
+svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
