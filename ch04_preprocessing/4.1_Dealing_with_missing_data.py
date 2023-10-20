06a40e9f828cafb4fd8670534620eaee0d90268e
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-21 07:27:44 +0900

    finish 4.1.2

--- ch04_preprocessing/4.1_Dealing_with_missing_data.py
+++ ch04_preprocessing/4.1_Dealing_with_missing_data.py
@@ -15,3 +15,18 @@ print('------')
 print('欠陥一覧')
 print(df.isnull().sum())
 print('------')
+print('4.1.2-欠陥値を持つ訓練データ/特徴量を取り除く')
+# 欠陥地を含む行を排除
+print(df.dropna())
+print('------')
+# 欠陥地を含む列を排除
+print(df.dropna(axis=1))
+print('------')
+# すべての列がNaNである行だけ排除(すべての値がNaNである行はないため、配列全体が返される)
+print(df.dropna(how='all'))
+print('------')
+# 非NaN値が4つ未満の行を削除
+print(df.dropna(thresh=4))
+print('------')
+# 特定の列(この場合は'C')にNaNが服割れている行だけを削除
+print(df.dropna(subset=['C']))
