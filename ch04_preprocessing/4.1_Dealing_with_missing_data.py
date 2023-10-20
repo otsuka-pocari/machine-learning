2d99c843b9cf70cfda6fd8c72ad805fc15762761
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-21 07:19:11 +0900

    finish 4.1.1

--- ch04_preprocessing/4.1_Dealing_with_missing_data.py
+++ ch04_preprocessing/4.1_Dealing_with_missing_data.py
@@ -1,3 +1,17 @@
-
+import pandas as pd
+from io import StringIO
 
 print('4.1.1-表形式のデータで欠陥値を特定する')
+# サンプルデータを作成
+csv_data = '''A,B,C,D
+            1.0,2.0,3.0,4.0
+            5.0,6.0,,8.0
+            10.0,11.0,12.0'''
+# サンプルデータを読み込む
+df = pd.read_csv(StringIO(csv_data))
+print(df)
+print('------')
+# 各特徴量の欠陥をカウント
+print('欠陥一覧')
+print(df.isnull().sum())
+print('------')
