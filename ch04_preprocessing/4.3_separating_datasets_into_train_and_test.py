4736073eec8d6c81ad19cda09bfe9987beae184b
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-22 13:57:20 +0900

    4.3 done

--- ch04_preprocessing/4.3_separating_datasets_into_train_and_test.py
+++ ch04_preprocessing/4.3_separating_datasets_into_train_and_test.py
@@ -1,1 +1,22 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# wineデータセットを読み込む
+df_wine = pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+        header=None)
+# 列名を設定
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity od ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'proline']
+# クラスラベルを表示
+print('Class labels', np.unique(df_wine['Class label']))
+print(df_wine.head())
+
+# 特徴量とクラスラベルを別々に抽出
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+# 訓練データとテストデータに分割(全体の30%をテストデータにする)
+X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
