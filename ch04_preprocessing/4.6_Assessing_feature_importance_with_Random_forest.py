f8e94f7d6866a730e9ebc93e59b0a68e5627dcce
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-26 00:49:33 +0900

    finish ch04

--- ch04_preprocessing/4.6_Assessing_feature_importance_with_Random_forest.py
+++ ch04_preprocessing/4.6_Assessing_feature_importance_with_Random_forest.py
@@ -1,1 +1,51 @@
-print('hello world')
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+# 前提コード
+df_wine = pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+        header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity od ash', 'Magnesium', 'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'proline']
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
+
+# # Wineデータセットの特徴量の名称
+feat_labels = df_wine.columns[1:]
+# ランダムフォレストオブジェクトの生成(決定木の個数=500)
+forest = RandomForestClassifier(n_estimators=500, random_state=1)
+# モデルを適合
+forest.fit(X_train, y_train)
+# 特徴量の重要度を抽出
+importances = forest.feature_importances_
+# 重要度の降順で特徴量のインデックスを抽出
+indices = np.argsort(importances)[::-1]
+# 重要度の降順で特徴量の名称、重要度を表示
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" %
+          (f + 1, 30, feat_labels[indices[f]], importances[indices[f]]))
+
+plt.title('feature Importances')
+plt.bar(range(X_train.shape[1]), importances[indices], align='center')
+plt.xticks(range(X_train.shape[1]), feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
+
+# 特徴量選択オブジェクトを生成(重要度のしきい値を0.1に設定)
+sfm = SelectFromModel(forest, threshold=0.1, prefit=True)
+# 特徴量を抽出
+X_selected = sfm.transform(X_train)
+print('Number of features that meet this threshold criterion', X_selected.shape[1])
+
+for f in range(X_selected.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,
+                            feat_labels[indices[f]], importances[indices[f]]))
