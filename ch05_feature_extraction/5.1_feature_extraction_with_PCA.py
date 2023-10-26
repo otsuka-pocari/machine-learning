4fdd11c0f1769d13299ffa9803f27e1be0b03dbb
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-10-27 00:56:44 +0900

    5.1.3 done

--- ch05_feature_extraction/5.1_feature_extraction_with_PCA.py
+++ ch05_feature_extraction/5.1_feature_extraction_with_PCA.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -20,3 +21,27 @@ X_test_std = sc.transform(X_test)
 cov_mat = np.cov(X_train_std.T)                  # 共分散行列を作成
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)  # 固有値と固有ベクトルを計算
 print('\nEigenvalues \n %s' % eigen_vals)
+print('------')
+
+print('5.1.3-全分散と説明分散')
+# figに保存
+fig = plt.figure(1)
+print('fig1')
+# 固有値を合計
+tot = sum(eigen_vals)
+# 分散説明率を計算
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+# 分散説明率の累積和を取得
+cum_var_exp = np.cumsum(var_exp)
+# 分散説明率の棒グラフを作成
+plt.bar(range(1,14), var_exp, alpha=0.5, align='center',
+        label='Individual explained variance')
+# 分散説明率の累積和の階段グラフを作成
+plt.step(range(1, 14), cum_var_exp, where='mid',
+         label='Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Proncipal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+print('------')
