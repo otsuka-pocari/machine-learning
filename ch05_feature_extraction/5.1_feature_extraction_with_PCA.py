9dfa7df19741e14271566675f510b134353c1044
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-01 00:53:06 +0900

    5.1.4 done

--- ch05_feature_extraction/5.1_feature_extraction_with_PCA.py
+++ ch05_feature_extraction/5.1_feature_extraction_with_PCA.py
@@ -42,11 +42,12 @@ plt.step(range(1, 14), cum_var_exp, where='mid',
 plt.ylabel('Explained variance ratio')
 plt.xlabel('Proncipal component index')
 plt.legend(loc='best')
-plt.tight_layout()
-plt.show()
 print('------')
 
 print('5.1.4-特徴量変換')
+# figに保存
+fig = plt.figure(2)
+print('fig2')
 # (固有値, 固有ベクトル)のタプルのリストを生成
 eigen_pairs = [(np.abs(eigen_vals[i]),eigen_vecs[:,i])
                for i in range(len(eigen_vals))]
@@ -54,3 +55,20 @@ eigen_pairs = [(np.abs(eigen_vals[i]),eigen_vecs[:,i])
 eigen_pairs.sort(key=lambda k: k[0], reverse=True)
 w= np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
 print('Matrix W:\n',w)
+
+print(X_train_std[0].dot(w))
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+# 「クラスラベル」「点の色」「点の種類」の組み合わせからなるリストを生成してプロット
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1],
+                c=c, label=l, marker=m)
+
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+plt.show()
+print('------')
