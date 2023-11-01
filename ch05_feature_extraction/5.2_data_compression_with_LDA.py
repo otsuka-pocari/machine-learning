20307b2672639551f173c8d72161cdd5b3b9adcf
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-01 14:37:29 +0900

    5.2.4 done

--- ch05_feature_extraction/5.2_data_compression_with_LDA.py
+++ ch05_feature_extraction/5.2_data_compression_with_LDA.py
@@ -55,4 +55,35 @@ print('Between-class scatter matrix: %sx%s' % (S_B.shape[0], S_B.shape[1]))
 print('------')
 
 print('5.2.4-新しい特徴量部分空間の線形判別を選択する')
-#
+# inv関数で逆行列、dot関数で行列積、eig関数で固有値を計算
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i])
+               for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+print('Eigenvalues in descending order:\n')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+# 固有値の実数部の総和を求める
+tot = sum(eigen_vals.real)
+# 分散説明率とその累積和を計算
+discr = [(i / tot) for i in sorted(eigen_vals.real, reverse=True)]
+cum_discr = np.cumsum(discr)
+# figに保存
+fig = plt.figure(1)
+print('fig1')
+plt.bar(range(1, 14), discr, alpha=0.5, align='center',
+        label='Individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid',
+         label='Cumulative "discriminability"')
+plt.ylabel('"Discriminability" ratio')
+plt.xlabel('Linear Discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+# 2つの固有ベクトルから変換行列を作成
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real,
+               eigen_pairs[1][1][:, np.newaxis].real))
+print('Matrix W:\n', w)
+print('------')
