39bd3cdc58fdd7bb9fcc14e7c89fafa8cb858beb
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-03 14:21:12 +0900

    can't so skip, i will do ch05

--- ch05_feature_extraction/5.3_nonlinear_mapping_with_KPCA.py
+++ ch05_feature_extraction/5.3_nonlinear_mapping_with_KPCA.py
@@ -1,11 +1,54 @@
-from sklearn.datasets import make_moons
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+from sklearn.datasets import make_moons
+from sklearn.decomposition import PCA
+from rbf_kernel_pca import rbf_kernel_pca
 
-# figに保存
-fig = plt.figure(1)
+print('fig1:半月円の分離\n')
 # 2つの半月形データを作成してプロット
-X, y = make_moon(n_samples=100, random_state=123)
+X, y = make_moons(n_samples=100, random_state=123)
 plt.scatter(X[y==0, 0], X[y==0, 1], color='red', marker='^', alpha=0.5)
 plt.scatter(X[y==1, 0], X[y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+scikit_pca =PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+# グラフの数と配置、サイズを指定
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+# 1番目のグラフ領域に散布図をプロット
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1],
+              color='blue', marker='o', alpha=0.5)
+# 2番目のグラフ領域に散布図をプロット
+ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02,
+              color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_xlabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.tight_layout()
+plt.show()
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1],
+              color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02,
+              color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02,
+              color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_xlabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
 plt.tight_layout()
 plt.show()
--- ch05_feature_extraction/rbf_kernel_pca.py
+++ ch05_feature_extraction/rbf_kernel_pca.py
@@ -1,7 +1,10 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from scipy.spatial.distance import pdist, squareform
 from scipy import exp
 from scipy.linalg import eigh
 import numpy as np
+import pandas as pd
 
 def rbf_kernel_pca(X, gamma, n_components):
     """RBFカーネルPCAの実装
@@ -22,6 +25,26 @@ def rbf_kernel_pca(X, gamma, n_components):
         射影されたデータセット
 
     """
-
     # M×M次元のデータセットでペアごとのユークリッド距離の2乗を計算
+    sq_dists = pdist(X, 'sqeuclidean')
 
+    # ベアごとの距離を正方行列に変換
+    mat_sq_dists = squareform(sq_dists)
+
+    # 対称カーネル行列を計算
+    K = exp(-gamma * mat_sq_dists)
+
+    # カーネル行列を中心化
+    N = K.shape[0]
+    one_n = np.ones((N,N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # 中心化されたカーネル行列から固有対を取得:scipy.linalg.eignはそれらを昇順で返す
+    eigvals, eigvecs = eigh(K)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+    print('eigen_vecs\'s shape:', eigvecs.shape)
+
+    # 上位k個の固有ベクトル(射影されたデータ点)を収集
+    X_pc = np.column_stack((eigvecs[:, :i] for i in range(n_components)))
+
+    return X_pc
