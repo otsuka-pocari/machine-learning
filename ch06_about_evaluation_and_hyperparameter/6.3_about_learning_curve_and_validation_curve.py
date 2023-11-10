bc2d0844e21ae205cd0680361684846a8933e202
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-10 09:16:23 +0900

    6.3 done

--- ch06_about_evaluation_and_hyperparameter/6.3_about_learning_curve_and_validation_curve.py
+++ ch06_about_evaluation_and_hyperparameter/6.3_about_learning_curve_and_validation_curve.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import learning_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import validation_curve
 
 # 前提コード
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
@@ -51,3 +52,38 @@ plt.legend(loc='lower right')
 plt.ylim([0.8, 1.03])
 plt.tight_layout()
 plt.show()
+print('------')
+
+print('6.3.2-検証曲線を使って科学周知学習不足を明らかにする')
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+# validation_curve関数によりモデルのパラメータを変化させ、交差検証による正解率を算出
+# clf__CはLogisticRegressionオブジェクトのパラメータ
+train_scores, test_scores = validation_curve(estimator=pipe_lr,
+                                             X=X_train, y=y_train,
+                                             param_name='logisticregression__C',
+                                             param_range=param_range, cv=10)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(param_range, train_mean,
+         color='blue', marker='o', markersize=5,
+         label='Training accuracy')
+plt.fill_between(param_range,
+                 train_mean + train_std, train_mean - train_std,
+                 alpha=0.15, color='blue')
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--', marker='s', markersize=5,
+         label='Validation accuracy')
+plt.fill_between(param_range,
+                 test_mean + test_std, test_mean - test_std,
+                 alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+plt.show()
+print('------')
