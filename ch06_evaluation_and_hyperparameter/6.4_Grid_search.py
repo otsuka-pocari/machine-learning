f2fb3315072d69571c253b008e04f6b4ea6bf716
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-13 15:42:18 +0900

    6,4 done

--- ch06_evaluation_and_hyperparameter/6.4_Grid_search.py
+++ ch06_evaluation_and_hyperparameter/6.4_Grid_search.py
@@ -1,4 +1,66 @@
-from sklearn.modeel_selection import GridSerchCV
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+import ssl
 
+# SSLの証明書の期限切れに対する処置
+ssl._create_default_https_context = ssl._create_unverified_context
 
+# 前提コード
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'breast-cancer-wisconsin/wdbc.data', header=None)
+
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+                                                    stratify=y, random_state=1)
+
+print('6.4.1-グリッドサーチを使ったハイパーパラメータのチューニング')
+pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [{'svc__C': param_range, 'svc__kernel':['linear']},
+              {'svc__C': param_range, 'svc__gamma': param_range,
+               'svc__kernel': ['rbf']}]
+# ハイパーパラメータ値のリストparam_gridを指定し、
+# グリッドサーチを行うGridSearchCVクラスをインスタンス化
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring='accuracy', cv=10, refit=True, n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+# モデルの最良スコアを出力
+print(gs.best_score_)
+
+# 最良となるパラメータ値を出力
+print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train,y_train)
+print('Test accuracy:%.3f' % clf.score(X_test, y_test))
+
+print('6.4.2-入れ子式の交差検証によるアルゴリズムの選択')
+gs = GridSearchCV(estimator=pipe_svc,
+                  param_grid=param_grid,
+                  scoring='accuracy', cv=2)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+# ハイパーパラメータ値として決定機の深さをパラメータを指定し、
+# グリッドサーチを行うGridSearchCVクラスをインスタンス化
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0),
+                  param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                  scoring='accuracy', cv=2)
+scores = cross_val_score(gs,
+                         X_train,y_train,
+                         scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
