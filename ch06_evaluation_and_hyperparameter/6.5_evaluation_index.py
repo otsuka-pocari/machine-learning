b38f622a01e28a0309c9828bd6703ceda9fc7c65
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-13 16:11:58 +0900

    rest

--- ch06_evaluation_and_hyperparameter/6.5_evaluation_index.py
+++ ch06_evaluation_and_hyperparameter/6.5_evaluation_index.py
@@ -1,5 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score, f1_score
+from sklearn.metrics import make_scorer, f1_score
+import ssl
 
+# SSLの証明書の期限切れに対する処置
+ssl._create_default_https_context = ssl._create_unverified_context
 
+# 前提コード
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'breast-cancer-wisconsin/wdbc.data', header=None)
 
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+                                                    stratify=y, random_state=1)
+
+pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
+# 本コード
+print('6.5.1-混合行列を解釈する')
 pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+# テストと予測のデータから混合行列を生成
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+# 図のサイズを指定
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+# matshow関数で行列からヒートマップを描画
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')   # 件数を表示
+
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.tight_layout()
+plt.show()
+print('------')
+
+print('6.5.2-分類モデルの適合率と再現率を最適化する')
+# 適合率、再現率、F1スコアを出力
+print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+# カスタムの性能指標を出力
+c_gamma_range = [0.01, 0.1, 1.0, 10.0]
+param_grid = [{'svc__C':c_gamma_range, 'svc__kernel':['linear']},
+              {}]
