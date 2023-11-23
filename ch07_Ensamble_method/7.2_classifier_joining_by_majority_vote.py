29fa49d5b647a12bbd235c881feb9fa9265cb745
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-11-23 15:56:39 +0900

    7.2 doing

--- ch07_Ensamble_method/7.2_classifier_joining_by_majority_vote.py
+++ ch07_Ensamble_method/7.2_classifier_joining_by_majority_vote.py
@@ -1,4 +1,14 @@
 import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from MajorityVoteClassifier import MajorityVoteClassifier
 
 print('7.2.1-単純な多数決分類器を実装する')
 np.argmax(np.bincount([0, 0, 1], weights=[0.2, 0.2, 0.6]))
@@ -8,3 +18,47 @@ ex = np.array([[0.9, 0.1],
 p = np.average(ex, axis=0, weights=[0.2, 0.2, 0.6])
 print(p)
 print(np.argmax(p))
+print('-------')
+
+print('7.2.2-多数決の原理に基づいて予測を行う')
+iris = datasets.load_iris()
+X, y = iris.data[50:, [1, 2]], iris.target[50:]
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.5, random_state=1, stratify=y)
+
+clf1 = LogisticRegression(penalty='l2', C=0.001,
+                          solver='lbfgs',
+                          random_state=1)
+clf2 = DecisionTreeClassifier(max_depth=1,
+                              criterion='entropy',
+                              random_state=1)
+clf3 = KNeighborsClassifier(n_neighbors=1,
+                            p=2,
+                            metric='minkowski')
+pipe1 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf3]])
+clf_labels = ['Logistic regression', 'Decision tree', 'KNN']
+print('10-fold cross validation:\n')
+for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
+    print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
+    print("ROC AUC: %0.2f (+/- %0.2f) [%s]"
+          % (scores.mean(), scores.std(), label))
--- ch07_Ensamble_method/MajorityVoteClassifier.py
+++ ch07_Ensamble_method/MajorityVoteClassifier.py
@@ -35,6 +35,101 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
 
     def fit(sself, X, y):
         """ 分類機を学習させる
-        
+
+        パラメータ
+        ----------
+        X : {arraay-like, sparse matrix}, shape = [n_examples, n_features]
+
+        y : array-like, shape = [n_examples]
+
+        戻り値
+        -------
+        self : object
 
         """
+        if self.vote not in ('probability', 'classlabel'):
+            raise ValueError("vote must be 'probability'"
+                             "or 'classlabel'; got (vote=%r)" % self.vote)
+
+        if self.weights and len(self.weights) != len(self.classifiers):
+            raise ValueError("Number of classifiers and weights must be equal; "
+                             "got %d weights, %d classifiers"
+                             % (len(self.weights), len(self.classifiers)))
+            # LabelEncoderを使ってクラスラベルが0から始まるようにエンコードする
+            # self.predictのnp.argmax呼び出しで重要になる
+            self.lablenc_ = LabelEncoder()
+            self.lablenc_.fit(y)
+            self.classifiers_ = []
+            for clf in self.classifiers:
+                fitted_clf = clone(clf).fit(X, self.lablenc_.tranform(y))
+                self.classifiers_.append(fitted_clf)
+            return self
+
+    def predict(self, X):
+        """ Xのクラスラベルを予測する
+
+        パラメータ
+        ----------
+        X :{array-like, sparse matrix}, shape = [n_examples, n_features]
+            訓練データからなる行列
+
+        戻り値
+        ----------
+        maj_vote : array-like, shape = [n_examples]
+            予測されたクラスラベル
+
+        """
+        if self.vote == 'probability':
+            maj_vote = np.arxmax(self.predict_proba(X), axis=1)
+        else: # 'classlabel'での多数決
+
+            # clf.predict呼び出しの結果を収集
+            predictions = np.asarray([clf.predict(X)
+                                       for clf in self.classifier_]).T
+
+            # 各データ点のクラス確率に重みを掛け合わせた値が最大となる
+            # 列番号を配列として返す
+            maj_vote = np.apply_along_axis(
+                    lambda x:
+                    np.argmax(np.bincount(x, weights=self.weights)),
+                    axis=1,
+                    arr=predictions)
+
+            # 各データ点に確率の最大値を与えるクラスラベルを抽出
+            maj_vote = self.labl;enc_.inverse_transform(maj_vote)
+            return maj_vote
+
+    def predict_proba(self, X):
+        """ Xのクラス確率を予測する
+
+        パラメータ
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+             訓練ベクトル:n_examplesはデータ点の個数、n_featuresは特徴量の個数
+
+        戻り値
+        ---------
+        avg_proba : array-like, shape = [n_examples, n_classes]
+            各データ点に対する各クラスで重み付けた平均確率
+
+        """
+        avg_probas = np.asarray([clf.predict_proba(X)
+                                 for clf in self.classifiers_])
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
+        return avg_proba
+
+    def get_params(self, deep=True):
+        """ GridSearchの実行時に分類器のパラメータ名を取得"""
+        if not deep:
+            return super(MajorityVoteClassifier, self).get_params(deep=False)
+        else:
+            # キーを"分類器の名前__パラメータ名"、
+            # 値をパラメータの値とするディクショナリを生成
+            out = self.named_classifiers.copy()
+            for name, step in self.named_classifiers.items():
+                for key, value in step.get_params(deep=True).items():
+                    out['%s__%s' % (name, key)] = value
+
+            return out
+        """
+                    )
