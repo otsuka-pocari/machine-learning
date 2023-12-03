9578aa9606d5d29ba3e4a01e4eba56c1fd634304
otsuka-pocari <akinori.otsuka.55@gmail.com>
2023-12-04 00:45:16 +0900

    7.4 done

--- ch07_Ensamble_method/7.4_Bootstrap_AGGregatING.py
+++ ch07_Ensamble_method/7.4_Bootstrap_AGGregatING.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -43,3 +45,29 @@ y_test_pred = tree.predict(X_test)
 tree_train = accuracy_score(y_train, y_train_pred)
 tree_test = accuracy_score(y_test, y_test_pred)
 print('Decision tree train/test accuracies %.3f/%.3f' % (tree_train, tree_test))
+
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=1, ncols=2,
+                        sharex='col',
+                        sharey='row',
+                        figsize=(8, 3))
+for idx,clf, tt in zip([0, 1], [tree, bag], ['Decision tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train==0, 0],
+                       X_train[y_train==0, 1], c = 'blue', marker='^')
+    axarr[idx].scatter(X_train[y_train==1, 0],
+                       X_train[y_train==1, 1], c = 'green', marker='o')
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(0, -0.2, s='OD280/OD315 of diluted wines',
+         ha='center', va='center', fontsize=12, transform=axarr[1].transAxes)
+plt.show()
